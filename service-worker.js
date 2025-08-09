7e5a1d93890c143e8516a42a65662222974bca4d
Genady367 <galsik68@gmail.com>
2025-08-09 10:23:29 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,36 @@
-self.addEventListener('install', (event) => {
+const CACHE_NAME = 'bestdeal-cache-v3'; // увеличили версию
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/app.js',
+  '/js/feedback.js',
+  '/data.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+// Установка: кладём файлы в кэш
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('bestdeal-cache').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/css/style.css',
-        '/js/app.js',
-        '/manifest.json'
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting(); // сразу активируем новый SW
 });
 
-self.addEventListener('fetch', (event) => {
+// Активация: удаляем старые кэши
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(names =>
+      Promise.all(names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n)))
+    )
+  );
+  self.clients.claim(); // новый SW берёт вкладки под контроль
+});
+
+// Отдаём из кэша, либо тянем из сети
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(resp => resp || fetch(event.request))
   );
 });
